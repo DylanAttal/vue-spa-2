29ecc03a67c4b93645f7e3bd91c3ca39524d882a
Dylan Attal <attal.dylan@gmail.com>
2019-06-20 08:50:24 -0400

    Creates postsModule

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -10,35 +10,26 @@
 </template>
 <script>
 import Post from "./Post.vue";
-import appService from "../app.service.js";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
     "app-post": Post
   },
-  // eslint-disable-next-line space-before-function-paren
-  data() {
-    return {
-      id: this.$route.params.id,
-      posts: []
-    };
+  computed: {
+    ...mapGetters("postsModule", ["posts"])
   },
   methods: {
     loadPosts() {
       let categoryId = 2;
-      if (this.id === "front-end") {
+      if (this.$route.params.id === "mobile") {
         categoryId = 11;
-      } else {
-        this.posts = this.postsMobile;
       }
-      appService.getPosts(categoryId).then(data => {
-        this.posts = data;
-      });
+      this.$store.dispatch("postsModule/updateCategory", categoryId);
     }
   },
   watch: {
     $route(to, from) {
-      this.id = to.params.id;
       this.loadPosts();
     }
   },
